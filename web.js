9ed0191e5385d5714eea3e185bf1b990eb858695
bfisch02 <brett.fischler@tufts.edu>
2013-04-12 14:32:28 -0400

    GET Command added for home page

--- web.js
+++ web.js
@@ -16,6 +16,37 @@ app.all('/',function(req, res, next) {
 	next();
 });
 
+app.get('/',function(req,res){
+	app.set('Content-Type', 'text/html');
+	var myGames = new Array;
+	var gameString = '<link rel="stylesheet" href="web.css"><h1 style = "text-align:center">Highscores</h1><table style = "margin-left:auto;margin-right:auto;border:3px solid black"><tr><th style = "border: 1px solid black">Game Title</th><th style = "border: 1px solid black">Username</th><th style = "border: 1px solid black">Score</th><th style = "border: 1px solid black">Date</th></tr>';
+	numGames = 0;
+	db.games.find(function(err, games) {
+  		if(err || !games) console.log("NOTHING FOUND");
+ 	 	else games.forEach(function(gameFound) {
+  			myGames[numGames] = gameFound;
+  			numGames++;
+  		});
+  		myGames = myGames.sort(function(a,b){
+  			if (b.game_title>a.game_title)
+  				return -1;
+  			if (a.game_title>b.game_title)
+  				return 1;
+  			else
+  				return b.score-a.score;
+  			});
+  		for (i = 0; i<numGames; i++) {
+  			if (i<numGames) {
+  				gameString+='<tr><th style = "border: 1px solid black">'+myGames[i].game_title+'</th><th style = "border: 1px solid black">'+myGames[i].username+'</th><th style = "border: 1px solid black">'+myGames[i].score+'</th><th style = "border: 1px solid black">'+myGames[i].created_at+'</th><th></th></tr>';
+  			}
+  		}
+  		gameString+='</table>';
+  		res.send(gameString);
+  	});
+  	
+  	
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
