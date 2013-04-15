f22546aafbaeb5e69f287293725c9e877795bcf2
bfisch02 <brett.fischler@tufts.edu>
2013-04-14 23:15:00 -0400

    Corrected minor logic error

--- web.js
+++ web.js
@@ -21,8 +21,8 @@ app.get('/',function(req,res){
 	var gameString = '<h1 style = "text-align:center">Highscores</h1><table style = "margin-left:auto;margin-right:auto;border:3px solid black"><tr><th style = "border: 1px solid black">Game Title</th><th style = "border: 1px solid black">Username</th><th style = "border: 1px solid black">Score</th><th style = "border: 1px solid black">Date</th></tr>';
 	numGames = 0;
 	db.highscores.find(function(err, games) {
-  		if(err || !games) console.log("NOTHING FOUND");
- 	 	else games.forEach(function(gameFound) {
+  		if(err || !highscores) console.log("NOTHING FOUND");
+ 	 	else highscores.forEach(function(gameFound) {
   			myGames[numGames] = gameFound;
   			numGames++;
   		});
